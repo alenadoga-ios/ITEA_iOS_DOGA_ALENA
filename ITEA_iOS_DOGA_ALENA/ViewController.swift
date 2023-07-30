bde264867447212d16cd5ebab5b71b1cbe4a4a25
alenadoga-ios <127222938+alenadoga-ios@users.noreply.github.com>
2023-07-30 22:30:50 +0300

    Initial Commit

---
+++ ITEA_iOS_DOGA_ALENA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITEA_iOS_DOGA_ALENA
+//
+//  Created by Alena on 30.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
